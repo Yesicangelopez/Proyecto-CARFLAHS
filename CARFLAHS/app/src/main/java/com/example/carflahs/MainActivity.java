50daf039c24bbcc33de61a3aba81fb743823fe8d
Yesicagelopez <yesiangellopez45@gmail.com>
2021-06-24 11:19:22 -0500

    Archivo android,con modificaciones(creacion de splash, cambio de icono y color de la accion bar

---
+++ CARFLAHS/app/src/main/java/com/example/carflahs/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.carflahs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_CARFLAHS);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
