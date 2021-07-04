036c357f42f5944afbb3ab9e4baf41c04c2c47a7
Yesicagelopez <yesiangellopez45@gmail.com>
2021-07-04 18:10:00 -0500

    Configuracion a los botones para acceder a las nuevas activityscreadas

---
+++ CARFLAHS/app/src/main/java/com/example/carflahs/ActivityConductor.java
@@ -0,0 +1,14 @@
+package com.example.carflahs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conductor);
+    }
+}
--- CARFLAHS/app/src/main/java/com/example/carflahs/MainActivity.java
+++ CARFLAHS/app/src/main/java/com/example/carflahs/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.carflahs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        // configuración del boton conductor para pasar a la activity conductor
+
+        Button btnConductor;
+        btnConductor = findViewById(R.id.btnConductor);
+
+        btnConductor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ActivityConductor.class);
+                startActivity(intent);
+
+            }
+        });
+
+        // configuración del boton conductor para pasar a la activity pasajero
+
+        Button btnPasajero;
+        btnPasajero = findViewById(R.id.btnPasajero);
+
+        btnPasajero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ActivityPasajero.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
