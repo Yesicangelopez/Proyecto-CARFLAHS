c43b61ab6ed48051d3ea9c9a8c00341baa77911d
Yesicagelopez <yesiangellopez45@gmail.com>
2021-07-11 16:16:37 -0500

    Asignar acciones a botones y creacion de interfaz grafica de otras Activitys

--- CARFLAHS/app/src/main/java/com/example/carflahs/ActivityRegistro.java
+++ CARFLAHS/app/src/main/java/com/example/carflahs/ActivityRegistro.java
@@ -2,7 +2,11 @@ package com.example.carflahs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityRegistro extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class ActivityRegistro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Button btnCuenta;
+
+        btnCuenta = findViewById(R.id.btnCuenta);
+
+        btnCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityRegistro.this,ActivityRegistroVehiculo.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
