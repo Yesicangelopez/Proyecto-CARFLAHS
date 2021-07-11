d0f1a4650951ed94e82e28d7bd580f6868afc0f2
Yesicagelopez <yesiangellopez45@gmail.com>
2021-07-10 19:26:51 -0500

    Creacion de nuevas activitys y acciones al dar clic al TextView btnRegistrar

--- CARFLAHS/app/src/main/java/com/example/carflahs/ActivityConductor.java
+++ CARFLAHS/app/src/main/java/com/example/carflahs/ActivityConductor.java
@@ -2,13 +2,37 @@ package com.example.carflahs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityConductor extends AppCompatActivity {
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conductor);
+
+        // asignar una accion al TextView (btnRegistrar) para pasar ala activitivy registro
+
+        TextView btnRegistrar;
+
+        btnRegistrar = findViewById(R.id.btnRegistar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityConductor.this,ActivityRegistro.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
